e0e7e0cce3d5106947188214de83252d9873b49b
William Mc Cormack <wmccormack@hotmail.co.uk>
2017-02-02 19:27:36 +0000

    working for lowercase words with no spaces

--- MainClass.java
+++ MainClass.java
@@ -20,9 +20,38 @@ public class MainClass {
 	public MainClass() {
 		// TODO Auto-generated constructor stub
 	}
-	
-	public static void main(String[] args){
-		
+
+	public static void main(String[] args) {
+
+		Scanner myScanner = new Scanner(System.in);
+
+		String testWord;
+
+		System.out.println("please enter a word");
+
+		testWord = myScanner.nextLine();
+
+		System.out.println("***testing***");
+		System.out.println("***testing***");
+
+		if (wordTest(testWord)) {
+			System.out.println(testWord + " is a palindrome");
+
+		} else {
+			System.out.println(testWord + " is not a palindrome");
+		}
+
+	}
+
+	public static boolean wordTest(String testWord) {
+		char[] array = testWord.toCharArray();
+
+		for (int loop = 0, j = array.length - 1; loop < j; loop++, j--) {
+			if (array[loop] != array[j]) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
