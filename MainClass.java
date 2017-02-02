0447405be7e07dccf00cf5309c4002fa34de7521
William Mc Cormack <wmccormack@hotmail.co.uk>
2017-02-02 19:10:36 +0000

    first commit

---
+++ MainClass.java
@@ -0,0 +1,20 @@
+package wmccormack.palindrome_tester;
+
+/**
+ * this programme will take a user input word and test whether 
+ * it is a palindrome or not
+ * @author William
+ *
+ */
+public class MainClass {
+
+	/**
+	 * default constructor
+	 */
+	public MainClass() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
